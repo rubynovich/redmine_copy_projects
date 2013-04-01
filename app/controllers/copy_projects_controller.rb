b9910248efeeef78b5d0b990e02ef8b239b70e5c
rubynovich <roman@shipiev.me>
2013-04-01 15:57:50 +0400

    ops

--- app/controllers/copy_projects_controller.rb
+++ app/controllers/copy_projects_controller.rb
@@ -5,7 +5,13 @@ class CopyProjectsController < ApplicationController
   before_filter :find_user
 
   def copy_projects
-    @user.memberships << @user_from.memberships
+    Member.transaction do
+      @user_from.memberships.each do |m|
+        member = Member.new(:user => @user, :project => m.project)
+        member.roles << m.roles
+        member.save
+      end
+    end
     redirect_to edit_user_path(@user, :tab => 'memberships')
   end
 
