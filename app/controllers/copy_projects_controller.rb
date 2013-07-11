4de85b98a04c79016ca3f46642982ada9bddcbb6
rubynovich <roman@shipiev.me>
2013-07-11 16:43:23 +0400

    ops

--- app/controllers/copy_projects_controller.rb
+++ app/controllers/copy_projects_controller.rb
@@ -2,24 +2,40 @@ class CopyProjectsController < ApplicationController
   unloadable
   layout 'admin'
   before_filter :require_admin
-  before_filter :find_user
+  before_filter :find_user, only: :run
+  before_filter :find_user_from_id, only: :clear_all
 
   def run
     Member.transaction do
       @user_from.memberships.each do |m|
-        member = Member.new(:user => @user, :project => m.project)
+        member = Member.new(:principal => @user, :project => m.project)
         member.roles << m.roles
         member.save
       end
     end
-    redirect_to edit_user_path(@user, :tab => 'memberships')
+    redirect_to action: 'edit', controller: @user.class.to_s.underscore.pluralize, id: @user, tab: 'memberships'
+  end
+
+  def clear_all
+    groups = @user.groups.clone
+    @user.groups.clear
+    @user.memberships.clear
+    @user.groups = groups
+
+    redirect_to action: 'edit', controller: @user.class.to_s.underscore.pluralize, id: @user, tab: 'memberships'
   end
 
   private
 
   def find_user
-      @user_from = User.find(params[:user_from])
-      @user = User.find(params[:user_to])
+      @user_from = Principal.find(params[:user_from])
+      @user = Principal.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render_404
+  end
+
+  def find_user_from_id
+      @user = Principal.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       render_404
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 RedmineApp::Application.routes.draw do
 #  resources :copy_projects, :only => [:update]
   get 'users/:id/copy_projects/', :to => 'copy_projects#run'
+  get 'users/:id/copy_projects/clear_all', :to => 'copy_projects#clear_all'
 end
